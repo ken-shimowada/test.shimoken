ab352d9b4caa3ffadd106eaff7e9de60c1030cb3
ken_shimowada <ken.shimowada2@gmail.com>
2020-10-27 09:15:35 +0900

    ハンバーガーボタン実装

--- 201026 task-4/task-4 coding/js/script.js
+++ 201026 task-4/task-4 coding/js/script.js
@@ -5,3 +5,10 @@ $('.slider').slick({
     autoplaySpeed:5000,
     dots:true,
 });
+
+$('.burger-btn').on('click',function(){//.burger-btnをクリックすると
+    $('.burger-btn').toggleClass('close');
+    //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+    $('.header__inner__sp').toggleClass('open');
+    //.gnav__wrapperにopenクラスを付与
+});
